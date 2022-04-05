5e1cc88cbb6f14cce4f7bce79b4f62c12b64e227
ferutizi <fer.utizi@gmail.com>
2022-04-05 20:04:23 -0300

    calculadora interes compuesto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import Input from './components/Input'
+import Button from './components/Button'
+import Container from './components/Container'
+import Card from './components/Card'
+import Balance from './components/Balance'
+
+import { useState } from 'react'
+
+const compoundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit
+  for(let i = 0; i < years; i++) {
+    total = (total + contribution) * (rate + 1)
+  }
+
+  return Math.round(total)
+}
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
 
 function App() {
+  const [balance, setBalance] = useState('')
+
+  const handleSubmit = ({deposit, contribution, years, rate}) => {
+    const val = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    setBalance(formatter.format(val))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Card>
+        <Formik 
+        initialValues={{
+          deposit: '',
+          contribution: '',
+          years: '',
+          rate: '',
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={Yup.object({
+          deposit: Yup.number().required('Obligatorio').typeError('Debe ser un número'),
+          contribution: Yup.number().required('Obligatorio').typeError('Debe ser un número'),
+          years: Yup.number().required('Obligatorio').typeError('Debe ser un número'),
+          rate:
+          Yup.number()
+          .required('Obligatorio')
+          .typeError('Debe ser un número')
+          .min(0, 'El valor mínimo es 0')
+          .max(1, 'El valor máximo es 1'),
+        })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form>
+            <Input name="deposit" label="Depósito inicial"/>
+            <Input name="contribution" label="Contribución anual"/>
+            <Input name="years" label="Años"/>
+            <Input name="rate" label="Interés estimado"/>
+            <Button type='submit'>Calcular</Button>
+          </Form>
+        </Formik>
+        {balance !== '' ? <Balance>Balance final: {balance} </Balance> : null}
+      </Card>
+    </Container>
   );
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Card = styled.section`
+  background-color: #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width: 500px;
+  box-shadow: 0ox 2 px 3px rgba(0,0,0,0.3);
+`
+
+export default Card
